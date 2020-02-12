99686a2ef487ba79820031f91d3eb2e5c100191a
trojai <trojai@nist.gov>
2020-02-12 15:33:07 -0500

    Actor update 20200212T203301

--- js/sts-submission.js
+++ js/sts-submission.js
@@ -1,6 +1,6 @@
 
                 var stsAcceptingSubmission = false;
-                var stsIdleNodes = 0;
+                var stsIdleNodes = b'2\n';
                 var stsRunningNodes = 0;
                 var stsDownNodes = 0;
                 
