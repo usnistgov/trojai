52e2b235d809a4c12dbfa0ef2985cd3d00d2f1a7
trojai <trojai@nist.gov>
2020-06-08 13:55:09 -0400

    Actor update 20200608T175501

--- js/es-submission.js
+++ js/es-submission.js
@@ -1,8 +1,8 @@
 
                 var esAcceptingSubmission = false;
                 var esIdleNodes = 0;
-                var esRunningNodes = 0;
-                var esDownNodes = 0;
+                var esRunningNodes = 02;
+                var esDownNodes = 00;
                 
                  $(document).ready(function () {
                         $('#esIdleNodes').text(esIdleNodes);
