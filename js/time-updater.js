0c49c5dc62748d692691f967326d3533579aaccc
trojai <trojai@nist.gov>
2020-10-14 05:10:06 -0400

    Actor update 20201014T091001

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-                    var uploadTimestamp = 1602666001;
+                    var uploadTimestamp = 1602666601;
                     var d = new Date(0);
                     d.setUTCSeconds(uploadTimestamp);
                     var acceptingSubmissions = true; 
