327a455a6eb9068de0334ba59553ebc63639402d
trojai <trojai@nist.gov>
2021-04-13 12:30:27 -0400

    Actor update 20210413T163002

--- js/es-submission.js
+++ js/es-submission.js
@@ -1,7 +1,7 @@
 
                 var esAcceptingSubmission = true;
-                var esIdleNodes = 0;
-                var esRunningNodes = 5;
+                var esIdleNodes = 2;
+                var esRunningNodes = 3;
                 var esDownNodes = 0;
                 
                  $(document).ready(function () {
