6889c8986ce0fb5e9e9fd6c9f1da7cbb2c2e1301
Tim Blattner <timothy.blattner@nist.gov>
2022-08-08 09:22:55 -0400

    First commit, merge nist-pages

---
+++ js/es-submission.js
@@ -0,0 +1,13 @@
+
+            var esAcceptingSubmission = true;
+            var esIdleNodes = 1;
+            var esRunningNodes = 0;
+            var esDownNodes = 0;
+            
+             $(document).ready(function () {
+                    $('#esIdleNodes').text(esIdleNodes);
+                    $('#esRunningNodes').text(esRunningNodes);
+                    $('#esDownNodes').text(esDownNodes);
+                    $('#esAcceptingSubmission').text(esAcceptingSubmission);
+                });
+            
---
+++ js/time-updater.js
@@ -0,0 +1,10 @@
+
+                var uploadTimestamp = 1659964235;
+                var d = new Date(0);
+                d.setUTCSeconds(uploadTimestamp);
+                var acceptingSubmissions = true; 
+                
+                $(document).ready(function () {
+                    $('#timestamp').text(d.toISOString().split('.')[0] );
+                });
+                
