d7f090b429429027f6a6438178a03c126dad0153
Tim Blattner <timothy.blattner@nist.gov>
2022-08-09 15:09:43 -0400

    Actor update 20220809T190943

---
+++ js/sts-submissions.js
@@ -0,0 +1,13 @@
+
+               var stsAcceptingSubmission = true;
+               var stsIdleNodes = 1;
+               var stsRunningNodes = 1;
+               var stsDownNodes = 2;
+    
+                $(document).ready(function () {
+                       $('#stsIdleNodes').text(stsIdleNodes);
+                       $('#stsRunningNodes').text(stsRunningNodes);
+                       $('#stsDownNodes').text(stsDownNodes);
+                       $('#stsAcceptingSubmission').text(stsAcceptingSubmission);
+                   });
+               
