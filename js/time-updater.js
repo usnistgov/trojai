61f22cef15a631669e3bb487901da339259bc600
Tim Blattner <timothy.blattner@nist.gov>
2022-08-09 20:20:07 -0400

    Actor update 20220810T002007

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-                   var uploadTimestamp = 1660090527;
+                   var uploadTimestamp = 1660090807;
                    var d = new Date(0);
                    d.setUTCSeconds(uploadTimestamp);
                    var acceptingSubmissions = true; 
