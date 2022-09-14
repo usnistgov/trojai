6167dd88dc42615b26f63c7b1e82299a6d0273a0
tjb3 <timothy.blattner@nist.gov>
2022-09-14 17:26:16 -0400

    Actor update 20220914T212615

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-var uploadTimestamp = 1663190727;
+var uploadTimestamp = 1663190775;
 var d = new Date(0);
 d.setUTCSeconds(uploadTimestamp);
 var acceptingSubmissions = false; 
--- js/trojai-table-init.js
+++ js/trojai-table-init.js
@@ -9,7 +9,7 @@ $('#object-detection-aug2022-train-results').dataTable({ order: [[ sort_col, 'as
 sort_col = $('#object-detection-aug2022-train-all-results').find("th:contains('Cross Entropy')")[0].cellIndex;
 $('#object-detection-aug2022-train-all-results').dataTable({ order: [[ sort_col, 'asc' ]] });
 
-sort_col = $('#object-detection-aug2022-train-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#object-detection-aug2022-train-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-train-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
 sort_col = $('#object-detection-aug2022-test-results').find("th:contains('Cross Entropy')")[0].cellIndex;
@@ -18,16 +18,16 @@ $('#object-detection-aug2022-test-results').dataTable({ order: [[ sort_col, 'asc
 sort_col = $('#object-detection-aug2022-test-all-results').find("th:contains('Cross Entropy')")[0].cellIndex;
 $('#object-detection-aug2022-test-all-results').dataTable({ order: [[ sort_col, 'asc' ]] });
 
-sort_col = $('#object-detection-aug2022-test-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#object-detection-aug2022-test-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
-sort_col = $('#object-detection-aug2022-sts-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#object-detection-aug2022-sts-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-sts-results').dataTable({ order: [[ sort_col, 'desc' ]] });
 
-sort_col = $('#object-detection-aug2022-sts-all-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#object-detection-aug2022-sts-all-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-sts-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
 
-sort_col = $('#object-detection-aug2022-sts-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#object-detection-aug2022-sts-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-sts-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
 sort_col = $('#image-classification-aug2020-test-results').find("th:contains('Cross Entropy')")[0].cellIndex;
@@ -36,7 +36,7 @@ $('#image-classification-aug2020-test-results').dataTable({ order: [[ sort_col, 
 sort_col = $('#image-classification-aug2020-test-all-results').find("th:contains('Cross Entropy')")[0].cellIndex;
 $('#image-classification-aug2020-test-all-results').dataTable({ order: [[ sort_col, 'asc' ]] });
 
-sort_col = $('#image-classification-aug2020-test-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
+sort_col = $('#image-classification-aug2020-test-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
 $('#image-classification-aug2020-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
 $('.dataTables_length').addClass('bs-select');
