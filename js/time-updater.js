7d618d4cf8bf104c5d9d02959e01dce88db0ed56
tjb3 <timothy.blattner@nist.gov>
2022-09-14 17:13:05 -0400

    Actor update 20220914T211304

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-var uploadTimestamp = 1663188912;
+var uploadTimestamp = 1663189984;
 var d = new Date(0);
 d.setUTCSeconds(uploadTimestamp);
 var acceptingSubmissions = false; 
--- js/trojai-table-init.js
+++ js/trojai-table-init.js
@@ -30,15 +30,6 @@ $('#object-detection-aug2022-sts-all-results').dataTable({ order: [[ sort_col, '
 sort_col = $('#object-detection-aug2022-sts-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
 $('#object-detection-aug2022-sts-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
-sort_col = $('#image-classification-aug2020-train-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-train-results').dataTable({ order: [[ sort_col, 'asc' ]] });
-
-sort_col = $('#image-classification-aug2020-train-all-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-train-all-results').dataTable({ order: [[ sort_col, 'asc' ]] });
-
-sort_col = $('#image-classification-aug2020-train-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-train-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
-
 sort_col = $('#image-classification-aug2020-test-results').find("th:contains('Cross Entropy')")[0].cellIndex;
 $('#image-classification-aug2020-test-results').dataTable({ order: [[ sort_col, 'asc' ]] });
 
@@ -47,15 +38,6 @@ $('#image-classification-aug2020-test-all-results').dataTable({ order: [[ sort_c
 
 sort_col = $('#image-classification-aug2020-test-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
 $('#image-classification-aug2020-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
-
-sort_col = $('#image-classification-aug2020-sts-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-sts-results').dataTable({ order: [[ sort_col, 'desc' ]] });
-
-sort_col = $('#image-classification-aug2020-sts-all-results').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-sts-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
-
-sort_col = $('#image-classification-aug2020-sts-jobs').find("th:contains('Execution Timestamp')")[0].cellIndex;
-$('#image-classification-aug2020-sts-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 
 $('.dataTables_length').addClass('bs-select');
 });
