d5023a67824d12888e101364954b907d846734b5
tjb3 <timothy.blattner@nist.gov>
2022-09-14 11:09:15 -0400

    Actor update 20220914T150914

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,8 +1,8 @@
 
-var uploadTimestamp = 1662586317;
+var uploadTimestamp = 1663168154;
 var d = new Date(0);
 d.setUTCSeconds(uploadTimestamp);
-var acceptingSubmissions = true; 
+var acceptingSubmissions = false; 
 
 $(document).ready(function () {
    $('#timestamp').text(d.toISOString().split('.')[0] );
