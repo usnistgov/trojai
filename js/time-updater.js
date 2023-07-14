2890a6d2e477767eb27cddcde4651c7dbb8d23bd
trojai <trojai@nist.gov>
2023-07-14 16:28:30 -0400

    Actor update 20230714T202825

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-    var uploadTimestamp = 1689299491;
+    var uploadTimestamp = 1689366505;
     var d = new Date(0);
     d.setUTCSeconds(uploadTimestamp);
     var acceptingSubmissions = true; 
--- js/trojai-table-init.js
+++ js/trojai-table-init.js
@@ -207,6 +207,42 @@ if ($('#object-detection-feb2023-dev-jobs').find("th:contains('Submission Timest
 {  sort_col = $('#object-detection-feb2023-dev-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
   $('#object-detection-feb2023-dev-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 }
+if ($('#rl-lavaworld-jul2023-train-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-train-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-train-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-train-all-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-train-all-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-train-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-train-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-train-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-train-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-test-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-test-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-test-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-test-all-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-test-all-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-test-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-test-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-test-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-sts-results').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-sts-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-sts-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-sts-all-results').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-sts-all-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-sts-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#rl-lavaworld-jul2023-sts-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#rl-lavaworld-jul2023-sts-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#rl-lavaworld-jul2023-sts-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
 if ($('#image-classification-jun2020-test-results').find("th:contains('Cross Entropy')").length > 0)
 {  sort_col = $('#image-classification-jun2020-test-results').find("th:contains('Cross Entropy')")[0].cellIndex;
   $('#image-classification-jun2020-test-results').dataTable({ order: [[ sort_col, 'asc' ]] });
