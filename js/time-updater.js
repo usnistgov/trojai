e43b0d92dc03df63aea19d2e78a4ebbd9b5ce0f6
trojai <trojai@nist.gov>
2024-04-10 16:20:49 -0400

    Actor update 20240410T202045

--- js/time-updater.js
+++ js/time-updater.js
@@ -1,5 +1,5 @@
 
-    var uploadTimestamp = 1712609020;
+    var uploadTimestamp = 1712780445;
     var d = new Date(0);
     d.setUTCSeconds(uploadTimestamp);
     var acceptingSubmissions = true; 
--- js/trojai-table-init.js
+++ js/trojai-table-init.js
@@ -447,6 +447,54 @@ if ($('#cyber-network-c2-mar2024-dev-jobs').find("th:contains('Submission Timest
 {  sort_col = $('#cyber-network-c2-mar2024-dev-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
   $('#cyber-network-c2-mar2024-dev-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 }
+if ($('#llm-pretrain-apr2024-train-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-train-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-train-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-train-all-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-train-all-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-train-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-train-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-train-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-train-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-test-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-test-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-test-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-test-all-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-test-all-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-test-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-test-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-test-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-sts-results').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-sts-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-sts-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-sts-all-results').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-sts-all-results').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-sts-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-sts-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-sts-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-sts-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-dev-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-dev-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-dev-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-dev-all-results').find("th:contains('ROC-AUC')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-dev-all-results').find("th:contains('ROC-AUC')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-dev-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
+if ($('#llm-pretrain-apr2024-dev-jobs').find("th:contains('Submission Timestamp')").length > 0)
+{  sort_col = $('#llm-pretrain-apr2024-dev-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
+  $('#llm-pretrain-apr2024-dev-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
+}
 if ($('#image-classification-jun2020-test-results').find("th:contains('Cross Entropy')").length > 0)
 {  sort_col = $('#image-classification-jun2020-test-results').find("th:contains('Cross Entropy')")[0].cellIndex;
   $('#image-classification-jun2020-test-results').dataTable({ order: [[ sort_col, 'asc' ]] });
