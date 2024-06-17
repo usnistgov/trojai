943fa9803ebb20e7b81757a0a88e232af851d490
trojai <trojai@nist.gov>
2024-06-17 11:30:35 -0400

    Actor update 20240617T153023

--- js/sts-submission.js
+++ js/sts-submission.js
@@ -1,5 +1,5 @@
 
-            var stsAcceptingSubmission = true;
+            var stsAcceptingSubmission = false;
             var stsIdleNodes = 0;
             var stsRunningNodes = 0;
             var stsDownNodes = 1;
--- js/time-updater.js
+++ js/time-updater.js
@@ -1,8 +1,8 @@
 
-    var uploadTimestamp = 1718637663;
+    var uploadTimestamp = 1718638223;
     var d = new Date(0);
     d.setUTCSeconds(uploadTimestamp);
-    var acceptingSubmissions = true; 
+    var acceptingSubmissions = false; 
     
     $(document).ready(function () {
        $('#timestamp').text(d.toISOString().split('.')[0] );
