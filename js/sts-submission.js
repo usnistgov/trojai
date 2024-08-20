89dd3ac8dcc387585fe5d119ac8f8d5366412209
trojai <trojai@nist.gov>
2024-08-20 14:24:02 -0400

    Actor update 20240820T182345

--- js/sts-submission.js
+++ js/sts-submission.js
@@ -1,8 +1,8 @@
 
             var stsAcceptingSubmission = true;
-            var stsIdleNodes = 1;
+            var stsIdleNodes = -1;
             var stsRunningNodes = 0;
-            var stsDownNodes = 0;
+            var stsDownNodes = 2;
 
             $(document).ready(function () {
                    $('#stsIdleNodes').text(stsIdleNodes);
